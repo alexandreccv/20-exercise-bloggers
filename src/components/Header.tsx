12beeb068681b49e96fab75d626e8cb9c8acf198
Alexandre Carvalho <alex380bb@gmail.com>
2024-08-14 15:32:16 -0300

    requisito 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import '../styles/Header.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header>
       <h2>Bloggers</h2>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/about">
+          About
+        </NavLink>
+      </nav>
     </header>
   );
 }
