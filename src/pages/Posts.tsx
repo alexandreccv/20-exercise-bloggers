62f7b17337d15a93386a5c7a8bb3786f58c3f3f9
Alexandre Carvalho <alex380bb@gmail.com>
2024-08-14 17:09:33 -0300

    requisito 4

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Post from '../components/Post';
 import { posts } from '../data';
@@ -5,7 +6,11 @@ import { PostType } from '../types';
 import '../styles/Posts.css';
 
 export default function Posts() {
-  const userPosts: PostType[] = []; // TODO: recebe os posts do usuário selecionado
+  const { id } = useParams<{ id: string }>();
+
+  // Verifica se o id é definido e filtra os posts
+  const userPosts: PostType[] = id ? posts
+    .filter((post) => post.userId === parseInt(id, 10)) : [];
 
   return (
     <div data-testid="posts-page">
